4cae6d05f644454735dc3d304e675ef12f5b8747
Jaydeep <jaydipc95@gmail.com>
2022-05-18 14:56:11 -0500

    save

--- user-panel/user-side/src/app/navbar/navbar.component.ts
+++ user-panel/user-side/src/app/navbar/navbar.component.ts
@@ -6,30 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-  nav: any;
-  dropdown: any;
-  dropdownToggle: any;
-  navToggle: any;
 
-  constructor() {}
+  constructor() { }
 
   ngOnInit(): void {
-    this.nav = document.querySelector('nav');
-    this.dropdown = this.nav.querySelector('.dropdown');
-    this.dropdownToggle = this.nav.querySelector(
-      "[data-action='dropdown-toggle']"
-    );
-    this.navToggle = this.nav.querySelector("[data-action='nav-toggle']");
-  }
-  ondropClick() {
-    this.dropdown.classList.contains('show')
-      ? this.dropdown.classList.remove('show')
-      : this.dropdown.classList.add('show');
+
   }
 
-  OnNavToggle() {
-    this.nav.classList.contains('opened')
-      ? this.nav.classList.remove('opened')
-      : this.nav.classList.add('opened');
-  }
 }
